5f1b3631c015a207c2937e37d07f8e116ab36150
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-16 02:41:28 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,13 +1,11 @@
 def reverse_each_word(str)
-  new_arr = str.split
   final_arr = []
-  new_arr.each{|words|final_arr << "#{words.reverse}"}
+  new_arr = str.split.each{|words|final_arr << "#{words.reverse}"}
   final_arr.join(" ")
 end
 
 def reverse_each_word(str)
-  new_arr = str.split
   final_arr = []
-  new_arr.collect{|words|final_arr << "#{words.reverse}"}
+  new_arr = str.split.collect{|words|final_arr << "#{words.reverse}"}
   final_arr.join(" ")
 end
