d33c0a1cc2cdf92debbc2b34080f7e1a055d85c4
Rommel Haynes <rhaynes3k@gmail.com>
2019-11-16 02:41:11 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ end
 
 def reverse_each_word(str)
   final_arr = []
-  new_arr = str.split.collect{|words|final_arr << "#{words.reverse}.join"}
-  #final_arr.join(" ")
+  new_arr = str.split.collect{|words|final_arr << "#{words.reverse}"}
+  final_arr.join(" ")
 end
